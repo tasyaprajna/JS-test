6fbe241f0716f5a961004e6da8cda0c23df6fd2b
tasyaprajna <prajna.tasya@binus.ac.id>
2021-04-07 11:49:18 +0700

    pembelajaran mengenai array for each

--- script.js
+++ script.js
@@ -1,6 +1,49 @@
-console.log('Hello World');
+// array for each
 
-var myName = "Prajna"
-const mySecondName = "Tasya"
+//ambil element
+const skills = document.getElementById("skills");
 
-console.log(myName)
+//buat variabel
+const mySkills = ["ES6", "UX Design", "Git", "Java"];
+mySkills.push("Dev"); //kalau ditambahin gabakal error (push, pop)
+
+//mySkills = ["C++"]; //tapi kalau valuenya diganti semua bakal error
+
+console.log(mySkills);
+
+//ES6
+//foreach 
+// mySkills.forEach(function(mySkill) { //iterator function
+//     console.log(mySkill);
+// });
+
+//version 2 pisah function
+// function showSkill(mySkill) {
+//     console.log(mySkill);
+// }
+
+// mySkills.forEach(showSkill);
+
+
+//version 3
+// mySkills.forEach((mySkill) => { //iterator function
+//     console.log(mySkill);
+// });
+
+//version 4 pisah function
+const showSkill = (mySkill) => {
+    console.log(mySkill);
+}
+
+mySkills.forEach(showSkill);
+
+//version 5
+let parent = "<ul>";
+
+mySkills.forEach((mySkill) => {
+    parent += `<li>${mySkill}</li>`;
+});
+
+parent += "</ul>";
+
+skills.innerHTML = parent;
