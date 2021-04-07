a2e12316eb0fb359c70841d54d4a6b748ce5cd43
tasyaprajna <prajna.tasya@binus.ac.id>
2021-04-07 10:54:42 +0700

    pemasukkan perbedaan antara var dengan let

--- script.js
+++ script.js
@@ -1,6 +1,31 @@
-console.log('Hello World');
+// ES5
+var myName = "Prajna";
+myName = "Tasya";
 
-var myName = "Prajna"
-const mySecondName = "Tasya"
+console.log(myName);
 
-console.log(myName)
+
+var myCarsVar = ["BMW", "Tesla", "Honda"];
+
+for (var i = 0; i < myCarsVar.length; i++) {
+    console.log("My Cars var = " + myCarsVar[i]);
+}
+
+console.log(i);
+
+
+var myCarsLet = ["BMW", "Tesla", "Honda"];
+
+for (let x = 0; x < myCarsLet.length; x++) {
+    console.log("My Cars var = " + myCarsLet[x]);
+    console.log(x);
+}
+
+
+
+//ES6
+
+let myNameLet = "Prajna";
+myNameLet = "Regine";
+
+console.log(myNameLet);
