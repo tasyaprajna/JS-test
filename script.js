8980d922dcda76af969b95811e8fe8ae2cce3dc2
tasyaprajna <prajna.tasya@binus.ac.id>
2021-04-07 13:45:07 +0700

    pembelajaran fetch

--- script.js
+++ script.js
@@ -1,6 +1,32 @@
-console.log('Hello World');
+//ambil elemeent
+const products = document.getElementById('products');
 
-var myName = "Prajna"
-const mySecondName = "Tasya"
 
-console.log(myName)
+const allProducts = "https://fakestoreapi.com/products";
+
+function createNode(element) {
+    return document.createElement(element);
+}
+
+function append(parent, el) {
+    return parent.appendChild(el);
+}
+
+fetch(allProducts)
+    .then(res => res.json())
+    .then(json => {
+        json.map((product) => {
+            let li = createNode("li");
+            let img = createNode("img");
+            let span = createNode("span");
+
+            img.setAttribute("width", "200");
+
+            img.src = product.image;
+            span.innerHTML = `${product.title}, ${product.price}`;
+            append(li, img);
+            append(li, span);
+            append(products, li);
+        })
+    })
+    .catch(error => console.log(error)); //buat tampilin errornya apa
