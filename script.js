9bebda08a88e4242c3a8d80b6ca0372e10677f65
tasyaprajna <prajna.tasya@binus.ac.id>
2021-04-07 13:20:50 +0700

    pembelajaran mengenai map dan filter

--- script.js
+++ script.js
@@ -1,6 +1,26 @@
-console.log('Hello World');
+// ambil element
+const skills = document.getElementById('skills');
+const primarySkills = document.getElementById('primarySkills');
 
-var myName = "Prajna"
-const mySecondName = "Tasya"
+const mySkills = ["ES6", "Git", "UX Design"];
 
-console.log(myName)
+//map
+const printSkillsMap = mySkills.map((skill) => {
+    return skill;
+});
+skills.innerHTML = printSkillsMap;
+
+
+//foreach
+const printSkillsForEach = mySkills.forEach((skill) => { //undefined
+    return skill;
+});
+// skills.innerHTML = printSkillsForEach;
+
+
+//filter
+const printPrimarySkills = mySkills.filter((skill) => {
+    return skill != "Git";
+});
+
+primarySkills.innerHTML = printPrimarySkills;
