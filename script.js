cbc443fe250835ccbc156afebf7b68e044f048e7
tasyaprajna <prajna.tasya@binus.ac.id>
2021-04-07 11:06:55 +0700

    penambahan string concatenation

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
-console.log('Hello World');
+// console.log('Hello World');
 
-var myName = "Prajna"
-const mySecondName = "Tasya"
+// var myName = "Prajna"
+// const mySecondName = "Tasya"
 
-console.log(myName)
+// console.log(myName)
+
+const user = document.getElementById('user');
+
+//ES5
+const user_logged_in = "Prajna";
+user.innerHTML = "Welcome, " + user_logged_in;
+
+
+//ES6
+// user.innerHTML = 'Selamat datang, ${ user_logged_in }';
+
+user.innerHTML = `Anda berhasil login ${user_logged_in}`;
